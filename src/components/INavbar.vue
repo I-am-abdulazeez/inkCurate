90903b2db4ad7a750fe9b9dc90276d734e6fb8a6
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-04 10:29:50 +0100

    feat: add INavbar component for navigation links and improved header layout

---
+++ src/components/INavbar.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+import { useRoute } from "vue-router";
+import { computed } from "vue";
+
+const navigationLinks = [
+  { name: "About", path: "/about" },
+  { name: "Insights", path: "/insights" },
+];
+
+const route = useRoute();
+
+const isActiveRoute = computed(() => (path: string) => {
+  return route.path === path;
+});
+</script>
+
+<template>
+  <header
+    class="fixed top-0 left-0 right-0 z-50 bg-gray-950 backdrop-blur-sm podca"
+  >
+    <nav class="container mx-auto px-4 py-4">
+      <div class="flex justify-between items-center">
+        <RouterLink
+          to="/"
+          class="flex items-center gap-3 hover:opacity-90 transition-opacity"
+        >
+          <img
+            src="../assets/logo-ink.svg"
+            alt="Ink Insights Logo"
+            class="w-8 h-8"
+          />
+          <span class="text-white sm:text-lg font-semibold mt-2"
+            >Ink Insights.</span
+          >
+        </RouterLink>
+
+        <!-- Navigation Links -->
+        <div class="flex items-center gap-8">
+          <RouterLink
+            v-for="link in navigationLinks"
+            :key="link.path"
+            :to="link.path"
+            class="text-gray-200 transition-colors text-sm font-medium px-1 py-2"
+            :class="{
+              'text-violet-300  font-semibold': isActiveRoute(link.path),
+            }"
+          >
+            {{ link.name }}
+          </RouterLink>
+        </div>
+      </div>
+    </nav>
+  </header>
+</template>
