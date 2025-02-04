0d3653373da7ff4af5a10720339a4c409292deb4
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-04 20:32:09 +0100

    feat: integrate Vue3 Toastify for enhanced toast notifications

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,10 @@
-import './assets/main.css'
+import './assets/main.css';
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
+import Vue3Toastify, { type ToastContainerOptions } from 'vue3-toastify';
+import 'vue3-toastify/dist/index.css';
 
 import App from './App.vue'
 import router from './router'
@@ -9,6 +12,17 @@ import router from './router'
 const app = createApp(App)
 
 app.use(createPinia())
+app.use(Vue3Toastify, {
+  autoClose: 3000,
+  position: 'bottom-center',
+  toastStyle: {
+    fontFamily: "Parkinsans, Inter, sans-serif",
+    borderRadius: "10px",
+    fontWeight: 500,
+    backgroundColor: "#1f2937",
+    color: "#f3f4f6",
+  },
+} as ToastContainerOptions,)
 app.use(router)
 
 app.mount('#app')
