2a4af484fbbf84c2d9c532afc02f4f85de79b50f
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-04 12:47:54 +0100

    refactor: simplify App.vue by removing unused imports and redundant logic

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-import { computed } from "vue";
-
-import { RouterView, useRoute } from "vue-router";
+import { RouterView } from "vue-router";
 import INavbar from "@/components/INavbar.vue";
-// import IFooter from "@/components/IFooter.vue";
-
-const route = useRoute();
-const isHome = computed(() => route.path === "/");
 </script>
 
 <template>
   <INavbar />
-  <main
-    :class="{ 'overflow-hidden': isHome, 'min-h-screen': !isHome }"
-    class="flex flex-col"
-  >
+  <main class="flex flex-col">
     <RouterView class="flex-grow" />
   </main>
   <!-- <IFooter /> -->
