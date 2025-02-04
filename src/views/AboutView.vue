ee579c05d9b1beef1ec6d71e3a759de138c31309
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-04 11:51:48 +0100

    feat: enhance About page layout and content for better user engagement

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,45 @@
 <template>
-  <div class="text-center">
-    <h1>This is an about page</h1>
-  </div>
+  <section class="flex flex-col justify-start my-16 px-4 h-[100vh] sm:h-[80vh]">
+    <div class="container mx-auto">
+      <h1 class="text-white text-3xl font-bold my-15">
+        About
+        <span class="text-violet-300 pencil-highlighter">Ink Insights</span>
+      </h1>
+
+      <div class="flex flex-col gap-10 sm:gap-20 sm:flex-row sm:items-center">
+        <img
+          src="../assets/reads.jpg"
+          alt="Ink Insights Logo"
+          class="w-full max-w-sm rounded-lg shadow-lg shadow-gray-700"
+        />
+        <article class="text-gray-300 self-start">
+          <p class="leading-relaxed text-[12px] sm:text-lg">
+            Finding the right book isn’t always easy. With countless stories
+            waiting to be discovered, how do you choose the one that truly
+            speaks to you? Ink Insights is an AI-powered book companion designed
+            to make that decision effortless. Instead of searching for a
+            specific title, simply type your thoughts—a mood, a theme, or an
+            idea—and let our AI guide you to the perfect read.
+          </p>
+          <p class="mt-4 leading-relaxed text-[12px] sm:text-lg">
+            Whether you crave a gripping mystery, a heartwarming romance, or a
+            thought-provoking journey, Ink Insights understands your preferences
+            and finds books that match your emotions. No more endless scrolling
+            or second-guessing—just instant, personalized recommendations
+            tailored to your reading desires.
+          </p>
+          <p class="mt-4 leading-relaxed text-[12px] sm:text-lg">
+            Our goal is to help you connect with stories that matter. Books are
+            more than words on a page; they are experiences, emotions, and
+            entire worlds waiting to be explored. With Ink Insights, every book
+            recommendation is designed to resonate with you on a deeper level.
+          </p>
+
+          <p class="mt-4 leading-relaxed text-[12px] sm:text-lg">
+            Now, go <span class="text-violet-200">read! 📖✨</span>
+          </p>
+        </article>
+      </div>
+    </div>
+  </section>
 </template>
