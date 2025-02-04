0b2c9a3333a6618c48a67b6067755768649b92de
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-04 12:16:56 +0100

    fix: increase font size in HomeView for improved readability

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -20,7 +20,7 @@ import Button from "@/components/ui/Button.vue";
       </div>
     </div>
     <p
-      class="text-gray-300 text-[12px] font-light sm:text-lg text-center my-10 max-w-sm sm:max-w-xl"
+      class="text-gray-300 text-[14px] font-light sm:text-lg text-center my-10 max-w-sm sm:max-w-xl"
     >
       <span class="text-violet-300">Ink Insights</span>
       is your personal book recommendation assistant, offering tailored
