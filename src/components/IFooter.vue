703853c9f86ce476477d42bc068e2feb6b85ee85
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-04 10:29:37 +0100

    feat: add IFooter component for consistent footer layout in application

---
+++ src/components/IFooter.vue
@@ -0,0 +1,8 @@
+<template>
+  <footer class="podca">
+    <h5 class="text-gray-200 text-center text-sm">
+      Maintained by
+      <RouterLink to="/test" class="text-violet-200">Abdul'Azeez</RouterLink>
+    </h5>
+  </footer>
+</template>
