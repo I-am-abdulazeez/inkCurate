5d7083da3e4fcad2dda213de203deae1c3a9baa7
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-04 10:30:29 +0100

    feat: update HomeView layout and integrate Button component for improved user interaction

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import AnimatedText from "@/components/AnimateText.vue";
+import Button from "@/components/ui/Button.vue";
 </script>
 
 <template>
-  <main class="flex justify-center items-center h-[85vh] flex-col">
-    <div class="flex gap-3 flex-col">
-      <h3 class="text-white text-5xl font-bold text-center">
+  <main class="flex justify-center items-center h-[95vh] flex-col podca">
+    <div class="flex gap-3 flex-col px-3 md:px-0">
+      <h3 class="text-white text-3xl md:text-5xl font-bold text-center">
         Type Your Thoughts,
       </h3>
       <div class="flex items-center">
@@ -16,16 +17,16 @@ import AnimatedText from "@/components/AnimateText.vue";
         />
       </div>
     </div>
-    <p class="text-gray-300 text-lg text-center my-10 max-w-xl">
-      <span>Ink Insights</span> is your personal book recommendation assistant,
-      offering tailored suggestions based on your thoughts and preferences.
+    <p
+      class="text-gray-300 text-[13px] font-light sm:text-lg text-center my-10 max-w-md sm:max-w-xl"
+    >
+      <span class="text-violet-300">Ink Insights</span>
+      is your personal book recommendation assistant, offering tailored
+      suggestions based on your thoughts and preferences.
     </p>
 
-    <RouterLink
-      to="/explore"
-      class="bg-violet-500 rounded-2xl font-semibold text-white px-6 py-3 hover:bg-violet-700 transition-colors"
-    >
+    <Button size="lg" color="violet" radius="2xl" to="/test" variant="solid">
       Get Started! 🚀
-    </RouterLink>
+    </Button>
   </main>
 </template>
