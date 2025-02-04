c1bf800a8a3515bbb7f303900361a75b86209ff7
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-04 10:27:35 +0100

    feat: add INavbar and IFooter components to App.vue for improved layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <script setup lang="ts">
 import { RouterView } from "vue-router";
+import INavbar from "@/components/INavbar.vue";
+import IFooter from "@/components/IFooter.vue";
 </script>
 
 <template>
+  <INavbar />
   <RouterView />
+  <IFooter />
 </template>
