2ad3e48355e58338cf5b5bd3c2ea9f31be236886
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-05 05:54:49 +0100

    fix: adjust margin for navigation links in INavbar component

--- src/components/INavbar.vue
+++ src/components/INavbar.vue
@@ -38,7 +38,7 @@ const isActiveRoute = computed(() => (path: string) => {
             v-for="link in navigationLinks"
             :key="link.path"
             :to="link.path"
-            class="text-gray-200 transition-colors text-sm font-medium px-1 py-2"
+            class="text-gray-200 mt-2 transition-colors text-sm font-medium px-1 py-2"
             :class="{
               'text-violet-300  font-semibold': isActiveRoute(link.path),
             }"
