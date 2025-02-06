c7652d07588c5d4916ad432b984966e5574ad979
AbdulAzeez <abdulazeezolamide50@gmail.com>
2025-02-06 17:58:01 +0100

    fix: update loading message and improve book title and description display for better readability

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -22,24 +22,21 @@ const goToBookDetails = (book: Book) => {
 
 <template>
   <div v-if="loading" class="text-gray-400 mt-6">
-    Fetching recommendations....
+    Fetching recommendations...
   </div>
 
   <div v-else-if="books.length" class="mt-12 mb-20">
-    <!-- Section Title -->
     <h2 class="text-xl sm:text-2xl text-white font-semibold">
       Recommended Reads
     </h2>
 
-    <!-- Divider -->
     <div class="w-16 h-1 bg-violet-300 mt-2 mb-6 rounded-full"></div>
 
-    <!-- Book Grid -->
     <div class="grid sm:grid-cols-2 gap-6">
       <div
         v-for="book in books"
         :key="book.link"
-        class="bg-gray-900 p-4 rounded-xl flex space-x-4 cursor-pointer"
+        class="bg-gray-900 p-4 rounded-xl flex space-x-4 cursor-pointer overflow-hidden"
         @click="goToBookDetails(book)"
       >
         <img
@@ -47,16 +44,17 @@ const goToBookDetails = (book: Book) => {
           :alt="book.title"
           class="w-24 h-32 object-cover rounded-lg flex-shrink-0"
         />
-
-        <div class="flex flex-col justify-between">
-          <div>
-            <h3 class="text-white text-lg font-semibold line-clamp-1">
+        <div class="flex flex-col justify-between overflow-hidden">
+          <div class="overflow-hidden">
+            <h3 class="text-white text-lg font-semibold truncate">
               {{ book.title }}
             </h3>
-            <p class="text-gray-400 text-sm line-clamp-1">
+            <p class="text-gray-400 text-sm truncate">
               {{ book.authors.join(", ") }}
             </p>
-            <p class="text-gray-300 text-xs mt-2 line-clamp-3">
+            <p
+              class="text-gray-300 text-xs mt-2 max-h-[3rem] overflow-hidden text-ellipsis"
+            >
               {{ book.description }}
             </p>
           </div>
